743928a47812fdb0bd314470cfae458bec36536c
Hunterly01 <mhunterly@gmail.com>
2025-12-18 13:06:17 +0100

    user rpository

---
+++ src/main/java/org/example/data/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package org.example.data.repositories;
+
+import org.example.data.models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmail(String email);
+}
