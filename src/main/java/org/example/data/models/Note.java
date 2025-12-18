a4b110d6b13739568bd7d498b90c12ce73e137c2
Hunterly01 <mhunterly@gmail.com>
2025-12-18 12:47:17 +0100

    notes managemet app only models done

---
+++ src/main/java/org/example/data/models/Note.java
@@ -0,0 +1,17 @@
+package org.example.data.models;
+
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Document("Notes")
+public class Note {
+    private String id;
+    private String title;
+    private String description;
+    private String content;
+    private LocalDateTime created = LocalDateTime.now();
+    private LocalDateTime updated = LocalDateTime.now();
+}
