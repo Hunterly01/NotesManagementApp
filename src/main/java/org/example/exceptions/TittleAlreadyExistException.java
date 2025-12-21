b0f98af4249b4edf11f5f5c90633423e3ce9ae90
Hunterly01 <mhunterly@gmail.com>
2025-12-21 13:58:35 +0100

    TittleAlreadyEcxeption

---
+++ src/main/java/org/example/exceptions/TittleAlreadyExistException.java
@@ -0,0 +1,7 @@
+package org.example.exceptions;
+
+public class TittleAlreadyExistException extends NoteManagementExceptions {
+    public TittleAlreadyExistException(String message) {
+        super(message);
+    }
+}
