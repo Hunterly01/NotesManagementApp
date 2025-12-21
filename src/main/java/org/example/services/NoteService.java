3fbfac5b90ca1a9993c4dbd2e83201a9de2e8511
Hunterly01 <mhunterly@gmail.com>
2025-12-21 16:20:37 +0100

    updateNote deleteNote viewAllNotes

--- src/main/java/org/example/services/NoteService.java
+++ src/main/java/org/example/services/NoteService.java
@@ -3,6 +3,11 @@ package org.example.services;
 import org.example.dtos.requests.NoteRequest;
 import org.example.dtos.responses.NoteResponse;
 
+import java.util.List;
+
 public interface NoteService {
     NoteResponse createNote(NoteRequest noteRequest);
+    NoteResponse updateNote(NoteRequest noteRequest);
+    void deleteNote(String tittle);
+    List<NoteResponse> viewAllNotes();
 }
